0f22310cd3d5b90af75a7625fff9e03ae8e10699
AttaUrRehman <atta.shafiq1278@gmail.com>
2024-04-24 20:13:29 +0500

    changes

--- shop/views.py
+++ shop/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world! This is a test.")
+    return render(request, 'shop/index.html')
