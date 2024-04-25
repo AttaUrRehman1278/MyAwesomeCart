2ef72e68ee5e83a37652bba990f5e4606f27c2dd
AttaUrRehman <atta.shafiq1278@gmail.com>
2024-04-25 21:09:54 +0500

    changes

--- shop/views.py
+++ shop/views.py
@@ -4,3 +4,21 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'shop/index.html')
+
+def about(request):
+    return HttpResponse("This is about")
+
+def contact(request):
+    return HttpResponse("This is contact us")
+
+def tracker(request):
+    return HttpResponse("This is Tracking Status")
+
+def search(request):
+    return HttpResponse("This is search")
+
+def productview(request):
+    return HttpResponse("This is product view")
+
+def checkout(request):
+    return HttpResponse("This is checkout")
